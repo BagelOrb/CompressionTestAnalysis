1574bfc7b5cecc02db24bf8da54a7d2a6ec6cd08
Tim Kuipers <t.kuipers@ultimaker.com>
2018-12-10 15:32:23 +0100

    fix axis labels and file naming
    split code into separate parts to fix axis labels etc
    fixed inversion error in _no_dithering labeling

--- main_derived_stats.py
+++ main_derived_stats.py
@@ -23,14 +23,9 @@ for dithering in [True, False]:
     top_color = 'blue' # if dithering else 'darkcyan'
     side_color = 'orange' # if dithering else 'red'
 
-    dither_str = ('_no' if dithering else '') + '_dither'
+    dither_str = ('_no' if not dithering else '') + '_dither'
 
     fig = pyplot.figure(int(dithering))
-    fig.tight_layout()
-    ax = pyplot.gca()
-    ax.autoscale()
-
-    fig = pyplot.figure(10 + int(dithering))
     fig.tight_layout()
     ax = pyplot.gca()
     ax.autoscale()
@@ -38,7 +33,6 @@ for dithering in [True, False]:
     for top in [True, False]:
         color = top_color if top else side_color
         label_stress = 'E_z' if top else 'E_{xy}'
-        label_modulus = '\\sigma_z' if top else '\\sigma_{xy}'
         # label = label + dither_str
         data_here = data[np.logical_and(data[:,0] == dithering, data[:,1] == top),:]
         densities = data_here[:, 2]
@@ -63,6 +57,43 @@ for dithering in [True, False]:
             avg_label = '\\bar' + label_stress
             pyplot.plot(densities_uniq, avg_stresses, color=color, label='$'+avg_label+'$', drawstyle ='steps-mid')
 
+    pyplot.figure(int(dithering))
+    ax = pyplot.gca()
+
+    for density in [10.0592231765546, 14.2258898432212, 20.1184463531091, 28.4517796864425]:
+        ax.axvline(density, 0, 1, linestyle='dashed', color='grey')  # plot all whole densities
+    ax.set_xlabel('Density (%)')
+    ax.set_ylabel('Plateau stress ($MPa$)')
+    ax.set_ylim([0.0, 0.62])
+    pyplot.legend()
+    fig.savefig('analysis/results/plateau_height_top_and_side' + dither_str +'.pdf', bbox_inches='tight')
+
+# pyplot.show()
+
+
+
+pyplot.clf()
+
+x_range = [np.min(data[:,2]), np.max(data[:,2])]
+
+for dithering in [True, False]:
+    top_color = 'blue' # if dithering else 'darkcyan'
+    side_color = 'orange' # if dithering else 'red'
+
+    dither_str = ('_no' if not dithering else '') + '_dither'
+
+    fig = pyplot.figure(10 + int(dithering))
+    fig.tight_layout()
+    ax = pyplot.gca()
+    ax.autoscale()
+
+    for top in [True, False]:
+        color = top_color if top else side_color
+        label_modulus = '\\sigma_z' if top else '\\sigma_{xy}'
+        # label = label + dither_str
+        data_here = data[np.logical_and(data[:,0] == dithering, data[:,1] == top),:]
+        densities = data_here[:, 2]
+
         pyplot.figure(10 + int(dithering))
         ax = pyplot.gca()
         youngs_moduli = data_here[:,3]
@@ -80,20 +111,9 @@ for dithering in [True, False]:
                 moduli = data_here[densities == density, 3]
                 avg_moduli.append(np.mean(moduli))
 
-            avg_label = '\\bar' + label_stress
+            avg_label = '\\bar' + label_modulus
             pyplot.plot(densities_uniq, avg_moduli, color=color, label='$'+avg_label+'$', drawstyle ='steps-mid')
 
-
-    pyplot.figure(int(dithering))
-    ax = pyplot.gca()
-
-    for density in [10.0592231765546, 14.2258898432212, 20.1184463531091, 28.4517796864425]:
-        ax.axvline(density, 0, 1, linestyle='dashed', color='grey')  # plot all whole densities
-    ax.set_xlabel('Density (%)')
-    ax.set_ylabel('Plateau stress ($MPa$)')
-    ax.set_ylim([0.0, 0.62])
-    pyplot.legend()
-    fig.savefig('analysis/results/plateau_height_top_and_side' + dither_str +'.pdf', bbox_inches='tight')
 
     pyplot.figure(10 + int(dithering))
     ax = pyplot.gca()
@@ -105,4 +125,4 @@ for dithering in [True, False]:
     pyplot.legend()
     fig.savefig('analysis/results/youngs_modulus_top_and_side' + dither_str +'.pdf', bbox_inches='tight')
 
-pyplot.show()
+# pyplot.show()
